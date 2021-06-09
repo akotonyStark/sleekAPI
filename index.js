b91c806a5e43ed69e7f05a195bec512cc48e9a66
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-06-09 07:47:38 +0000

    added swagger

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const productRouter = require('./src/routers/product_router')
 const categoryRouter = require('./src/routers/category_router')
 require('./src/db/mongoose')
 require('env-cmd')
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 
 
 const app = express()
 const port = process.env.PORT || 3001
+
+
+//swagger config
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Sleek Collections API',
+            version: '1.0.0'
+        }
+    },
+    apis: ['index.js']
+}
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 
 app.use(express.json())
--- src/routers/category_router.js
+++ src/routers/category_router.js
@@ -4,12 +4,21 @@ const Category = require('../models/category')
 const router = express.Router()
 
 // POST ADD CATEGORY
+
+/**
+ * @swagger
+ * /Category:
+ *      post:
+ *          description: Add new category
+ *          responses:
+ *              201: Success
+ */
 router.post('/category', async(req, res) => {
     const category = new Category(req.body)
 
     try {
         await category.save()
-        res.status(200).send(category)
+        res.status(201).send(category)
     } catch (e) {
         res.status(400).send(e)
     }
@@ -17,6 +26,15 @@ router.post('/category', async(req, res) => {
 
 
 // GET ALL CATEGORY
+
+/**
+ * @swagger
+ * /Category:
+ *      get:
+ *          description: Get All Categories
+ *          responses:
+ *              200: Success
+ */
 router.get('/category', async(req, res) => {
     const category = await Category.find({})
 
