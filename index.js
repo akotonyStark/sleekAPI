ef7a8861e09f9eb080d2a6296c02c56c215a8f57
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-06-09 11:18:26 +0000

    swagger update

--- index.js
+++ index.js
@@ -5,36 +5,11 @@ require('./src/db/mongoose')
 require('env-cmd')
 const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUI = require('swagger-ui-express')
+const swaggerOptions = require('./swagger.json');
 
 
 const app = express()
 const port = process.env.PORT || 3001
-
-
-//swagger config
-const swaggerOptions = {
-    swaggerDefinition: {
-        openapi: "3.0.0",
-        info: {
-            title: 'Sleek Collections API',
-            version: '1.0.0',
-            description: "This is a simple CRUD API application made with Express and documented with Swagger",
-        },
-        contact: {
-            name: "Akwaaba Evolution",
-            url: "https://akwaabaevolution.com",
-            email: "akwaabaevolution@email.com",
-        },
-    },
-    servers: [{
-            url: "http://localhost:3000/category",
-        },
-        {
-            url: "https://sleek-collections-api.herokuapp.com/category"
-        }
-    ],
-    apis: ['./src/routers/*.js']
-}
 
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
@@ -43,7 +18,6 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 app.use(express.json())
 app.use(productRouter)
 app.use(categoryRouter)
-    //app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
 app.listen(port, () => {
     console.log('Listening to sleek api on port: ', port)
